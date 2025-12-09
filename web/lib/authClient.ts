3064cc3b747ccabd5d009684d71c1e1327f0c9ba
hyoni91 <hyoni.green@gmail.com>
2025-12-09 15:39:36 +0900

    firebaseConfig 함수

--- web/lib/authClient.ts
+++ web/lib/authClient.ts
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { auth } from "./firebaseClient";
 import {
--- web/lib/firebaseClient.ts
+++ web/lib/firebaseClient.ts
@@ -1,21 +1,21 @@
-"use client"
+"use client";
 
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
+function getFirebaseConfig() {
+  return {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  };
+}
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!, 
-};
+const config = getFirebaseConfig();
 
-export const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+export const app = getApps().length ? getApps()[0] : initializeApp(config);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const storage = getStorage(app);
-
-
-
