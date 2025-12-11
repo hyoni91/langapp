b141ff434408047cb638531f96db523179d8bbba
hyoni91 <hyoni.green@gmail.com>
2025-12-11 15:34:54 +0900

    OK

--- web/lib/firebaseAdmin.ts
+++ web/lib/firebaseAdmin.ts
@@ -3,13 +3,17 @@ import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
 // 이미 초기화된 앱이 있으면 재사용
+
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
+
+
 if (!getApps().length) {
   initializeApp({
     credential: cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
       // 환경변수의 개행문자를 복원해줘야 함
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      privateKey
     }),
   });
 }
